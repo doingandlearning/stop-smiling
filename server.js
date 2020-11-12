fb6dc84de167d898fc747dd5a5f4a196f4a09118
Kevin Cunningham <kevin@kevincunningham.co.uk>
2020-11-12 20:07:09 +0000

    Adds procfile and port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const { get } = require('request')
+const PORT = process.env.PORT || 5000
 
 const app = express()
 
@@ -10,10 +11,8 @@ app.use(express.urlencoded({ extended: true }))
 const viewsDir = path.join(__dirname, 'views')
 app.use(express.static(viewsDir))
 app.use(express.static(path.join(__dirname, './public')))
-app.use(express.static(path.join(__dirname, '../images')))
-app.use(express.static(path.join(__dirname, '../media')))
-app.use(express.static(path.join(__dirname, '../../weights')))
-app.use(express.static(path.join(__dirname, '../../dist')))
+app.use(express.static(path.join(__dirname, './weights')))
+app.use(express.static(path.join(__dirname, './dist')))
 
 app.get('/', (req, res) => res.sendFile(path.join(viewsDir, 'webcamFaceExpressionRecognition.html')))
 
@@ -31,7 +30,7 @@ app.post('/fetch_external_image', async (req, res) => {
   }
 })
 
-app.listen(3000, () => console.log('Listening on port 3000!'))
+app.listen(PORT, () => console.log('Listening on port 3000!'))
 
 function request(url, returnBuffer = true, timeout = 10000) {
   return new Promise(function (resolve, reject) {
